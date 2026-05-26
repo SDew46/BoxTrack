3e4d45b246fda3a46f2e89a23157f9e32e357644
SDew <stephendewsnip@gmail.com>
2026-05-26 13:15:48 +0100

    Rebrand and updates

--- sw.js
+++ sw.js
@@ -1,20 +1,10 @@
-const CACHE = 'boxtrack-v4';
-const ASSETS = ['./index.html', './manifest.json', './icon.png'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS).catch(()=>{})));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('./index.html')))
-  );
-});
+const CACHE = 'boxtrack-v5';
+const ASSETS = ['/', '/BoxTrack/', '/BoxTrack/index.html', '/BoxTrack/manifest.json'];
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(()=>{})));
+self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+  if(!res||res.status!==200||res.type!=='basic') return res;
+  const clone = res.clone();
+  caches.open(CACHE).then(c=>c.put(e.request,clone));
+  return res;
+}).catch(()=>caches.match('/BoxTrack/index.html')))));
